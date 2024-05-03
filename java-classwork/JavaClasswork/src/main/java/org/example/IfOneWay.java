ba2ef91be22b6895896831f529ab27bbfdc8c0d2
NasiyraM <nasiyra.mccorkle@gmail.com>
2024-05-03 11:24:26 -0400

    add if one way demo

---
+++ java-classwork/JavaClasswork/src/main/java/org/example/IfOneWay.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class IfOneWay {
+
+        public static void main(String[] args) {
+                int mark = 80;
+                if (mark >= 80) {
+                    System.out.println("Well Done!");
+                    System.out.println("Keep it up!");
+                }  // close if body
+                System.out.println("Life goes on!");
+            } // close main method body
+
+
+        }
