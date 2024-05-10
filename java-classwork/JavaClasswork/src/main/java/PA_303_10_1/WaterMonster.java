3f354f0966a2a79ac671e157287bead55ba2522e
NasiyraM <nasiyra.mccorkle@gmail.com>
2024-05-10 08:10:19 -0400

    add water monster class

---
+++ java-classwork/JavaClasswork/src/main/java/PA_303_10_1/WaterMonster.java
@@ -0,0 +1,12 @@
+package PA_303_10_1;
+
+public class WaterMonster extends Monster {
+    public WaterMonster(String idNum) {
+        super(idNum);
+    }
+
+    @Override
+    String attack(){
+        return "Attack with Water!";
+    }
+}
