cf74875923559e422dbb8ea96e927a78e36c5103
NasiyraM <nasiyra.mccorkle@gmail.com>
2024-05-10 08:06:48 -0400

    add stone monster class

---
+++ java-classwork/JavaClasswork/src/main/java/PA_303_10_1/StoneMonster.java
@@ -0,0 +1,12 @@
+package PA_303_10_1;
+
+public class StoneMonster extends Monster {
+    public StoneMonster(String idNum) {
+        super(idNum);
+    }
+
+    @Override
+    String attack(){
+        return "Attack with Stone!";
+    }
+}
