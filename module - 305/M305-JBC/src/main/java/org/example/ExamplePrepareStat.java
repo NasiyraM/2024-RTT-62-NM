bff51406acb4fa4be2127234c0025b7660dd5f05
NasiyraM <nasiyra.mccorkle@gmail.com>
2024-06-10 00:03:48 -0400

    Remove unnecessary comments

--- module - 305/M305-JBC/src/main/java/org/example/ExamplePrepareStat.java
+++ module - 305/M305-JBC/src/main/java/org/example/ExamplePrepareStat.java
@@ -39,24 +39,3 @@ public class ExamplePrepareStat {
         conn.close();
     }
 }
-
-//public class ExamplePrepareStat{
-//    public static void main (String[] args)throw Exception {
-//        Class.forName("com.mysql.cj.jdbc.Driver");
-//        String url =  "jdbc:mysql://localhost:3306/classic_models";
-//        final String USER = "root";
-//        final String PASS = "MYPassword";
-//        //Connection conn = DriverManager.getConnection(url, USER , PASS );
-//        Connection connection = DriverManager.getConnection(dburl, user, password);
-//        String SelectSQL = "select * from employees where employeeNumber = ?";
-//        PreparedStatement mystmt = conn.prepareStatement(SelectSQL);
-//        mystmt.setInt(1, 1002);
-//        ResultSet result = mystmt.executeQuery();
-//        while(result.next())
-//        {
-//            String name = result.getString("firstName");
-//            String email  = result.getString("email");
-//            System.out.println(name +" | " + email);
-//        }
-//    }
-//}
