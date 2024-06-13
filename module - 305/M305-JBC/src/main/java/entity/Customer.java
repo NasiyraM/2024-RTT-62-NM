7f557e127245d352bb362410e31bc79080246744
NasiyraM <nasiyra.mccorkle@gmail.com>
2024-06-13 12:10:48 -0400

    Move Customer entity to entity package

---
+++ module - 305/M305-JBC/src/main/java/entity/Customer.java
@@ -0,0 +1,30 @@
+package entity;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "customers")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long customerNumber;
+
+    private String customerName;
+    private String contactLastName;
+    private String contactFirstName;
+    private String phone;
+    private String addressLine1;
+    private String addressLine2;
+    private String city;
+    private String state;
+    private String postalCode;
+    private String country;
+    private Double creditLimit;
+
+    @OneToMany(mappedBy = "customer")
+    private Set<Order> orders;
+
+    // Getters and Setters
+}
